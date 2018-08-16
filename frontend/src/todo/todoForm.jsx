3639b19a6895136f5f70838f7cb82a38bf4a9f40
Marcelo Abrão da Silva <masilvasql@gmail.com>
2018-08-16 19:13:54 -0300

    ajustes de componentes de botão e grid

--- frontend/src/todo/todoForm.jsx
+++ frontend/src/todo/todoForm.jsx
@@ -1,14 +1,13 @@
 import React from 'react'
-
-export default props =>(
+import Grid from '../template/grid'
+import IconButton from '../template/iconButon'
+export default props => (
     <div role='form' className='todoForm'>
-        <div className='col-xs-12 col-sm-9 col-md-10'>
+        <Grid cols='12 9 10'>
             <input id="description" className='form-control' placeholder="Adicione uma Tarefa"></input>
-        </div>
-        <div className='col-xs-12 col-sm-3 col-md-2'>
-            <button className='btn btn-primary'>
-                <i className='fa fa-plus'></i>
-            </button>
-        </div>
+        </Grid>
+        <Grid cols='12 3 2'>
+            <IconButton style='primary' icon='plus'></IconButton>
+        </Grid>
     </div>
 )
